b5745ee18a889401905a127a599ce6e48dc8c03e
Anders Bjerner <abjerner@skybrud.dk>
2021-08-06 03:16:22 +0200

    Updated package to build against Umbraco 9.0.0-rc001 😎

---
+++ src/Skybrud.Separator/SeparatorComposer.cs
@@ -0,0 +1,29 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.WebAssets;
+
+namespace Skybrud.Separator {
+    
+    public class SeparatorComposer : IComposer {
+        
+        public void Compose(IUmbracoBuilder builder) {
+            
+            builder.BackOfficeAssets().Append<SeparatorAssetFile>();
+
+        }
+
+    }
+
+    public class SeparatorAssetFile : IAssetFile {
+        
+        public string FilePath { get; set; }
+        
+        public AssetType DependencyType => AssetType.Css;
+
+        public SeparatorAssetFile() {
+            FilePath = "/App_Plugins/Skybrud.Separator/Styles/Default.css";
+        }
+
+    }
+
+}
--- src/Skybrud.Separator/SeparatorDataEditor.cs
+++ src/Skybrud.Separator/SeparatorDataEditor.cs
@@ -1,14 +1,10 @@
-﻿using ClientDependency.Core;
-using Umbraco.Core.Logging;
-using Umbraco.Core.PropertyEditors;
-using Umbraco.Web.PropertyEditors;
+﻿using Umbraco.Cms.Core.PropertyEditors;
 
 namespace Skybrud.Separator {
 
     /// <summary>
     /// Represents a separator property editor.
     /// </summary>
-    [PropertyEditorAsset(ClientDependencyType.Css, "/App_Plugins/Skybrud.Separator/Styles/Default.css")]
     [DataEditor(EditorAlias, EditorType.PropertyValue, "Skybrud Separator", EditorView, Group = "Skybrud.dk", Icon = "icon-navigation-horizontal", HideLabel = true)]
     public class SeparatorDataEditor : DataEditor {
 
@@ -16,7 +12,7 @@ namespace Skybrud.Separator {
 
         internal const string EditorView = "/App_plugins/Skybrud.Separator/Views/Separator.html";
 
-        public SeparatorDataEditor(ILogger logger) : base(logger) { }
+        public SeparatorDataEditor(IDataValueEditorFactory dataValueEditorFactory) : base(dataValueEditorFactory) { }
 
     }
 
